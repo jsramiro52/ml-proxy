35b542359b5822a1e1d8cdacad995b66a6840d87
jsramiro52 <jsramiro52@gmail.com>
2026-05-27 11:45:11 -0600

    Update search.js

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -1,5 +1,19 @@
 exports.handler = async (event) => {
-  const { q = '', limit = 50, offset = 0 } = event.queryStringParameters || {};
+  const { q = '', limit = 50, offset = 0, code = '' } = event.queryStringParameters || {};
+
+  if (code) {
+    const tokenRes = await fetch('https://api.mercadolibre.com/oauth/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: `grant_type=authorization_code&client_id=3842307979853353&client_secret=lRZQjSW2cdcwCalC0jswZbVCvAQz8dB2&code=${code}&redirect_uri=https://sprightly-nasturtium-0189de.netlify.app`
+    });
+    const tokenData = await tokenRes.json();
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(tokenData)
+    };
+  }
 
   if (!q) return {
     statusCode: 400,
