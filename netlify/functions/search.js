7f0002049fbc9ee096f0e8b8687353bbec216635
jsramiro52 <jsramiro52@gmail.com>
2026-05-27 11:17:03 -0600

    Update search.js

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -7,7 +7,15 @@ exports.handler = async (event) => {
   };
 
   const url = `https://api.mercadolibre.com/sites/MLM/search?q=${encodeURIComponent(q)}&limit=${limit}&offset=${offset}`;
-  const res = await fetch(url);
+  
+  const res = await fetch(url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'Accept': 'application/json',
+      'Accept-Language': 'es-MX,es;q=0.9'
+    }
+  });
+  
   const data = await res.json();
 
   return {
