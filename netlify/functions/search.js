9313f825d2bd340b6430f7a8acac589c79882558
jsramiro52 <jsramiro52@gmail.com>
2026-05-27 11:07:16 -0600

    Create search.js

---
+++ netlify/functions/search.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  const { q = '', limit = 50, offset = 0 } = event.queryStringParameters || {};
+
+  if (!q) return {
+    statusCode: 400,
+    body: JSON.stringify({ error: 'Falta el parámetro q' })
+  };
+
+  const url = `https://api.mercadolibre.com/sites/MLM/search?q=${encodeURIComponent(q)}&limit=${limit}&offset=${offset}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify(data)
+  };
+};
